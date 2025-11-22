490c4cf3c619084102d3322aa94a7c2ad302a454
booukes <lukaszandrzejpilat@gmail.com>
2025-11-22 18:55:19 +0100

    our-mission added

---
+++ app/layouts/course-edit.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import bg from '@/assets/images/course-edit.jpg';
+</script>
+
+<template>
+  <main
+      class="relative isolate w-svw h-full min-h-svh bg-center bg-cover p-4"
+      :style="{ backgroundImage: `url(${bg})` }">
+    <slot />
+  </main>
+</template>
